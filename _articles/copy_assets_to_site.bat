cb0fec97305312f0189bb0e37b8baa343f035a6f
June6699 <3439394104@qq.com>
2026-02-07 19:49:52 +0800

    Version2

--- _articles/copy_assets_to_site.bat
+++ _articles/copy_assets_to_site.bat
@@ -1,6 +1,6 @@
 @echo off
-REM 从 _articles 复制 *.assets 到 _site 里对应文章目录下，这样 HTML 里的相对路径 ./xxx.assets/ 才能找到图。
-REM 请先执行 bundle exec jekyll build，再运行本 bat（可双击或在项目根目录执行）。
+REM 从 _articles 复制 *.assets 到 _site 里对应文章目录下（路径与 Jekyll 输出一致，为小写）。
+REM 请先执行 bundle exec jekyll build，再运行本 bat。
 setlocal
 cd /d "%~dp0.."
 if not exist _site (
@@ -8,9 +8,9 @@ if not exist _site (
     exit /b 1
 )
 if not exist _site\_articles mkdir _site\_articles
-if not exist _site\_articles\HiCPlot mkdir _site\_articles\HiCPlot
-if not exist _site\_articles\Win11-Right-Click mkdir _site\_articles\Win11-Right-Click
-xcopy "%~dp0HiCPlot.assets" "_site\_articles\HiCPlot\HiCPlot.assets\" /E /I /Y >nul
-xcopy "%~dp0Win11_right_click.assets" "_site\_articles\Win11-Right-Click\Win11_right_click.assets\" /E /I /Y >nul
+if not exist _site\_articles\hicplot mkdir _site\_articles\hicplot
+if not exist _site\_articles\win11-right-click mkdir _site\_articles\win11-right-click
+xcopy "%~dp0HiCPlot.assets" "_site\_articles\hicplot\HiCPlot.assets\" /E /I /Y >nul
+xcopy "%~dp0Win11_right_click.assets" "_site\_articles\win11-right-click\Win11_right_click.assets\" /E /I /Y >nul
 echo [OK] 已复制文章图片到 _site\_articles\ 对应目录。
 endlocal
--- _plugins/copy_articles_assets.rb
+++ _plugins/copy_articles_assets.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 # 把每篇文章的 *.assets 复制到 _site 里该文章的目录下，这样相对路径 ./xxx.assets/ 才能正确显示图片。
-# 图片只在 _articles 下保留一份，不移动、不复制到仓库其他位置。
+# 图片只在 _articles 下保留一份。Jekyll 的 collection URL 会变成小写（如 hicplot），所以用 doc.url 得到真实路径。
 module Jekyll
   class CopyArticlesAssetsGenerator < Generator
     safe true
@@ -11,22 +11,20 @@ module Jekyll
       return unless articles
 
       src_root = File.join(site.source, "_articles")
-      dest_root = File.join(site.dest, "_articles")
       return unless File.directory?(src_root)
 
       articles.docs.each do |doc|
-        # 文章输出目录，如 _site/_articles/HiCPlot/
+        # 文章输出目录（Jekyll 会写成小写，如 _site/_articles/hicplot/）
         doc_path = doc.url.gsub(%r{^/}, "").sub(%r{/$}, "")
         doc_dest = File.join(site.dest, doc_path)
-        next unless File.directory?(doc_dest)
+        # generate 阶段 Jekyll 还没写入文件，目录不存在，需要先创建
+        FileUtils.mkdir_p(doc_dest)
 
-        # 资源目录名：front matter 里 assets_folder 或默认 文件名.assets
         folder = doc.data["assets_folder"] || "#{doc.basename}.assets"
         src_dir = File.join(src_root, folder)
         next unless File.directory?(src_dir)
 
         dest_assets = File.join(doc_dest, folder)
-        FileUtils.mkdir_p(File.dirname(dest_assets))
         FileUtils.cp_r(src_dir, dest_assets, remove_destination: true)
       end
     end
